09341510123038166aae15bfbee0b339d53eb0a0
John Charlton <john_charlton@sympatico.ca>
2016-02-07 11:29:03 -0700

    After changing user band don't reload the whole screen

---
+++ SetGenerator.UI/WebUI/Scripts/Shared/_LogonPartial.js
@@ -0,0 +1,38 @@
+﻿/*!
+ * Shared/_LayoutPartial.js
+ * Author: John Charlton
+ * Date: 2016-01
+ */
+
+; (function ($) {
+    shared._logonpartial = {
+        init: function (options) {
+            var config = {
+                controller: "",
+                actionName: ""
+            }
+
+            $.extend(config, options);
+
+            if (config.actionName === "Sets") {
+                $("#ddlUserBand").attr("disabled", "disabled");
+            } else {
+                $("#ddlUserBand").removeAttr("disabled");
+            };
+
+            $("#ddlUserBand").change(function() {
+                $.ajax({
+                    type: "POST",
+                    url: site.url + "Home/SetCurrentBand/",
+                    data: { bandId: $("#ddlUserBand").val() },
+                    dataType: "json",
+                    traditional: true,
+                    async: false,
+                    success: function () {
+                        window.location.href = site.url + config.controller;
+                    }
+                });
+            });
+        }
+    }
+})(jQuery);
